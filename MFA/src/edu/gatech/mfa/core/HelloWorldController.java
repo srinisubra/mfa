2673e0f0f13366022b9a0d4832c347a65896a7f1
nadirsaghar <nadirsaghar@2c2c9958-e08b-a0b5-f539-67890f1f073e>
2010-11-08 18:10:28 +0000

    Sample webapp

---
+++ MFA/src/edu/gatech/mfa/core/HelloWorldController.java
@@ -0,0 +1,21 @@
+package edu.gatech.mfa.core;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+public class HelloWorldController implements Controller {
+
+	
+	
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		System.out.println("Request received");
+		return new ModelAndView("helloWorld");
+	}
+}
---
+++ MFA/src/edu/gatech/mfa/core/Test.java
@@ -0,0 +1,13 @@
+package edu.gatech.mfa.core;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Test {
+
+	private static Log log = LogFactory.getLog(Test.class);
+	
+	public static void main(String[] args) {
+		log.info("We are inside the main method");
+	}
+}
