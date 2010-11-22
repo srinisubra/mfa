15da9a570b0a2f551c24d937655913e80985f8fc
amaysinghal <amaysinghal@2c2c9958-e08b-a0b5-f539-67890f1f073e>
2010-11-22 00:53:09 +0000

    Change to hashing function return string

--- MFAMobile/src/edu/gatech/mfautils/CryptiUtils.java
+++ MFAMobile/src/edu/gatech/mfautils/CryptiUtils.java
@@ -4,6 +4,8 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.http.auth.Credentials;
+
 public class CryptiUtils {
 
 	
@@ -13,12 +15,12 @@ public class CryptiUtils {
 			byte[] data = pass.getBytes(); 
 			m.update(data,0,data.length);
 			BigInteger i = new BigInteger(1,m.digest());
-			return String.format("%1$032X", i);
+			System.out.println(i.toString(16));
+			return i.toString(16);
         }
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
 	}
-	
 	
 }
