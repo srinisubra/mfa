72ebdd22c71bdd616ae1be661526270688da0cab
nadirsaghar <nadirsaghar@2c2c9958-e08b-a0b5-f539-67890f1f073e>
2010-11-22 19:08:53 +0000

    updated the CrptUtils class

--- MFA/src/edu/gatech/mfa/extn/qrcodeauth/CryptiUtils.java
+++ MFA/src/edu/gatech/mfa/extn/qrcodeauth/CryptiUtils.java
@@ -12,17 +12,26 @@ public class CryptiUtils {
 			MessageDigest m = MessageDigest.getInstance("SHA-1");
 			byte[] data = pass.getBytes(); 
 			m.update(data,0,data.length);
-			BigInteger i = new BigInteger(1,m.digest());
-			
-			return i.toString(16);
+			String retVal = prependZeroes(m.digest());
+			return retVal;
         }
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
 	}
 	
+	public static  String prependZeroes(byte[] bytes)
+	{
+		StringBuffer sb=new StringBuffer();
+		for (int i = 0; i < bytes.length; i++) {
+		    String hex=Integer.toHexString(0xff & bytes[i]);
+		    if(hex.length()==1) sb.append('0');
+		    sb.append(hex);
+		}
+		return sb.toString();
+	}
 	public static void main(String[] args) {
-		getMD5Hash("asdasda");
+		System.out.println(getMD5Hash("amay_nadir798325"));
 	}
 	
 }
